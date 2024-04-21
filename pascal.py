c4e65205e1444898dea1976b80b9bd66585332f2
PedPietro <166723178+PedPietro@users.noreply.github.com>
2024-04-21 18:24:15 -0300

    triaugulo de puscal
    fiz o triangulo finalmente

---
+++ pascal.py
@@ -0,0 +1,27 @@
+
+class Matematica:
+    def __init__(self, numeroBase):
+        self._numeroBase = numeroBase
+
+    def fatorial(self, x):
+        if x == 0:
+            return 1
+        else:
+            return x * self.fatorial(x - 1)
+
+    def triangulo_de_Pascal(self):
+        triangulo = []
+        for n in range(self._numeroBase):
+            linha = ""
+            for k in range(n + 1):
+                coeficiente = self.fatorial(n) // (self.fatorial(k) * self.fatorial(n - k))
+                linha += f"{coeficiente:6}"
+            triangulo.append(linha)
+        return triangulo
+        
+# Exemplo de uso
+numeroBase = 8
+matematica = Matematica(numeroBase)
+triangulo = matematica.triangulo_de_Pascal()
+for linha in triangulo:
+    print(linha)
